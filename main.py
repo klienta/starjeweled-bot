dba9eb9de2f4c42efc89adf8c18a8044e34ef0f7
Troy Thompson <troynt@gmail.com>
2011-05-09 18:31:37 -0400

    use starjeweled tiles by default

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ from strategy import *
 def main():
     """ load tiles """
     tiles = {}
-    for tile in glob.glob('gfx/bejeweled-blitz/tiles/*.bmp'):
+    
+    game = 'starjeweled'
+    tile_size = 51
+    
+    #uncomment for bejeweled blitz
+    #game = 'bejeweled-blitz'
+    #tile_size = 40
+    
+    for tile in glob.glob('gfx/' + game + '/tiles/*.bmp'):
       key, ext = os.path.basename(tile).split('.')
       tiles[key] = (cv.LoadImage(tile))
 
     b = Board()
-    b.setTileImages(tiles, 40, 40)
-    b.findBoard('gfx/bejeweled-blitz/top_left_corner.bmp')
+    b.setTileImages(tiles, tile_size, tile_size)
+    b.findBoard('gfx/' + game + '/top_left_corner.bmp')
     
     s = Strategy(b)
     while 1:
